bd9bbd91e347704c4697ab416790ea5b352c798d
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-22 17:37:40 +0600

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='min-h-screen '>
+        {/* theme toggle */}
+
+
+        {/* background effects */}
+
+
+        {/* navbar */}
+
+        {/* main content */}
+
+        {/* footer */}
+    </div>
+  )
+}
+
+export default Home
