d1ad4543b4d2ebec6066e1143eea6deda703576d
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-23 23:32:34 +0600

    some bugs

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { ArrowUp, ArrowUpIcon } from "lucide-react"
 
 const Footer = () => {
   return (
-    <footer className="py-12 px-4 bg-card relative border-t border-border mt-121 pt-8 flex flex-wrap justify-between items-center">
+    <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
         <p className="text-sm text-muted-foreground">&copy; {new Date().getFullYear()} Samriddha.co, All Right reserved</p>
       
       <a href="#hero">
