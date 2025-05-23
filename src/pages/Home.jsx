63d6df579000a4d732af7da36e00b92486b1d968
Samriddha-Dewan <dewansamriddha@gmail.com>
2025-05-23 16:09:03 +0600

    my skills section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import StarBackground from '../components/StarBackground'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/HeroSection'
 import AboutSection from '../components/AboutSection'
+import SkillsSection from '../components/SkillsSection'
 
 const Home = () => {
   return (
@@ -19,6 +20,7 @@ const Home = () => {
         <main>
           <HeroSection />
           <AboutSection />
+          <SkillsSection />
         </main>
         {/* footer */}
     </div>
